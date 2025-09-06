69b1e110fbdb46d32c09ae1917744c0ed8ed7a84
SqlRabbi <shimongb@gmail.com>
2025-09-07 00:06:39 +0300

    first commit #3

---
+++ MyCBMTests/MyCBMTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyCBMTests.swift
+//  MyCBMTests
+//
+//  Created by Shimon Gibraltar on 30/08/2025.
+//
+
+import Testing
+@testable import MyCBM
+
+struct MyCBMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
